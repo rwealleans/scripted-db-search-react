39ca4e22c4394803deedd5ba12e2983226d6c4e2
Joe Tessler <jrt@google.com>
2017-12-11 15:31:39 -0500

    Add firebase database starter code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import TestCases from './TestCases.js'
 import logo from './logo.svg';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    var config = {
+      apiKey: "AIzaSyBtrRNrWZNZrRfAXJYaSeZ1nkgxxOrp2ks",
+      authDomain: "scripted-org.firebaseapp.com",
+      databaseURL: "https://scripted-org.firebaseio.com",
+      projectId: "scripted-org",
+      storageBucket: "scripted-org.appspot.com",
+      messagingSenderId: "436190267084"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Firebase Database Find And Go Seek (in React!)</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/TestCases.js</code> and save to reload.
         </p>
+        <TestCases />
       </div>
     );
   }
---
+++ src/TestCases.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import './TestCases.css';
+
+class TestCases extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { testCaseElements: [] };
+  }
+
+  render() {
+    var database = firebase.database();
+
+    // TODO: Uncommen the following cade and update this Firebase database
+    // lookup to access the path "/db-search/test-cases".
+
+    /*
+    var testCasesDatabase = database.ref('<INSERT D.B. PATH HERE>');
+    testCasesDatabase.once('value').then(function(snapshot) {
+      var elements = [];
+      var testCases = snapshot.val();
+      for (var i = 0; i < testCases.length; i++) {
+        var testCase = testCases[i];
+        var testName = testCase['test-name'];
+        elements.push(
+          <div key={i} className="TestCase">
+            <b>{testName}</b>
+            <p>TODO: Add more test case items, e.g. test-hint, etc.</p>
+          </div>);
+      }
+      this.setState({ testCaseElements: elements });
+    }.bind(this));  // So we can access "this" in the callback above.
+    */
+
+    return (
+      <div className="TestCases">
+        {this.state.testCaseElements}
+      </div>
+    );
+  }
+}
+
+export default TestCases;
